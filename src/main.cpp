f06ae57215cd1684ee5c8e5f9c65a16eabac72a0
ubilby <ubilby@gmail.com>
2022-10-06 15:52:11 +0300

    add pms5003 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <ArxContainer.h>
 #include "myAgents.h"
 
-
 // (период, мс), (0 не запущен / 1 запущен), (режим: 0 период / 1 таймер)
-TimerMs tmr(1000, 1, 0);
+TimerMs tmr(5000, 1, 0);
+TimerMs tmr2(1000, 1, 0);
+
 
 // Класс наших наблюдений
 DataPoint dp;
@@ -20,15 +21,16 @@ arx::vector<std::shared_ptr<Sensor>> sensors;
 
 // Инициация агентов
 std::shared_ptr<Agent> serialAgent = std::make_shared<SerialAgent>();
+
 arx::vector<std::shared_ptr<Agent>> agents;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(115200);
-    // temperatureSensor->initiateSensor();
+    temperatureSensor->initiateSensor();
     pms5003Sensor->initiateSensor();
-    // sensors.push_back(temperatureSensor);
+    sensors.push_back(temperatureSensor);
     sensors.push_back(pms5003Sensor);
     agents.push_back(serialAgent);
 }
@@ -36,18 +38,18 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+
     if (tmr.tick()) {
-        for (auto sensor : sensors) {
-            Serial.println("1");
-            sensor->updateData();
-            Serial.println("2");
-            sensor->SaveDataToDataPoint(dp);
-            Serial.println("3");
-
+        for (unsigned int i =0; i < sensors.size(); i++) {
+            sensors[i]->updateData();
+            sensors[i]->SaveDataToDataPoint(dp);
         }
 
-        for(auto agent: agents) {
-            agent->sendData(dp);
+        for (unsigned int i =0; i < agents.size(); i++) {
+            agents[i]->sendData(dp);
         }
+        // pms5003Sensor->updateData();
+        // pms5003Sensor->SaveDataToDataPoint(dp);
+        // serialAgent->sendData(dp);
     }
 }
--- src/mySensors.h
+++ src/mySensors.h
@@ -1,5 +1,5 @@
 #include "Adafruit_SHTC3.h"
-#include <SoftwareSerial.h>
+#include <PMserial.h> // Arduino library for PM sensors with serial interface
 
 
 class DataPoint {
@@ -7,25 +7,14 @@ public:
     float temperature;
     float humidity;
 
-    uint16_t pm10_standard;
-    uint16_t pm25_standard;
-    uint16_t pm100_standard;
+    unsigned int pm10_standard;
+    unsigned int pm25_standard;
+    unsigned int pm100_standard;
 
-    uint16_t particles_03um;
-    uint16_t particles_05um;
-    uint16_t particles_10um;
+    unsigned int particles_03um;
+    unsigned int particles_05um;
+    unsigned int particles_10um;
 };
-
-struct pms5003data {
-  uint16_t framelen;
-  uint16_t pm10_standard, pm25_standard, pm100_standard;
-  uint16_t pm10_env, pm25_env, pm100_env;
-  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
-  uint16_t unused;
-  uint16_t checksum;
-};
-
-struct pms5003data data;
 
 
 class Sensor {
@@ -39,14 +28,12 @@ public:
 //temperature sensor SHTC3 sda - A4, scl A5
 class TemperatureSensor : public Sensor {
 public:
-    void updateData() {
-        Serial.println("6");
+    void updateData() override  {
         shtc3.getEvent(&humidity, &temp);
-        Serial.println("7");
 
     }
 
-    void initiateSensor() {
+    void initiateSensor() override  {
         if (! shtc3.begin()) {
             Serial.println("Couldn't find SHTC3");
             // while (1) delay(1);
@@ -56,7 +43,7 @@ public:
         }
     }
 
-    void SaveDataToDataPoint(DataPoint& dp) {
+    void SaveDataToDataPoint(DataPoint& dp) override  {
         dp.temperature = temp.temperature;
         dp.humidity = humidity.relative_humidity;
     }
@@ -71,97 +58,34 @@ private:
 };
 
 
-//pms5003 sensor
+//pms5003 sensor's rx - 3, tx - 2 
 class PMS5003Sensor : public Sensor {
 public:
-    PMS5003Sensor() : pmsSerial(2,3) {
+    PMS5003Sensor() : pms (PMS5003, 2, 3){
 
     }
 
-    void updateData() {
-        Serial.println("4");
-
-        if(readPMSdata(&pmsSerial)) {
-            Serial.println("5");
-
-            Serial.println("send to pms5003 sensor");
-        }
-        else {
-            Serial.println("coudn't send to pms5003 sensor");
-
-        }
+    void updateData() override {
+        pms.read();
     }
 
-    void SaveDataToDataPoint(DataPoint& dp) {
-        dp.pm10_standard = data.pm10_standard;
-        dp.pm25_standard = data.pm25_standard;;
-        dp.pm100_standard = data.pm100_standard;
+    void SaveDataToDataPoint(DataPoint& dp) override  {
+        dp.pm10_standard = pms.pm01 ;
+        dp.pm25_standard = pms.pm25;
+        dp.pm100_standard = pms.pm10;
 
-        dp.particles_03um = data.particles_03um;
-        dp.particles_05um = data.particles_05um;
-        dp.particles_10um = data.particles_10um;
+        dp.particles_03um = pms.n0p3;
+        dp.particles_05um = pms.n0p5;
+        dp.particles_10um = pms.n1p0;
     };
 
-    void initiateSensor() {
-        pmsSerial.begin(9600);
-        if (readPMSdata(&pmsSerial)) {
-            Serial.println("Found pms5003 sensor");
+    void initiateSensor() override  {
+        pms.init();
+        if (not pms) {
+            Serial.println("Couldn't find pms5003");
         }
-        else {
-            Serial.println("Couldn't Found pms5003 sensor");
-        };
     }
 
-    boolean readPMSdata(Stream *s) {
-        if (! s->available()) {
-            return false;
-        }
+    SerialPM pms;
 
-        // Read a byte at a time until we get to the special '0x42' start-byte
-        if (s->peek() != 0x42) {
-            s->read();
-            return false;
-        }
-
-        // Now read all 32 bytes
-        if (s->available() < 32) {
-            return false;
-        }
-
-        uint8_t buffer[32];
-        uint16_t sum = 0;
-        s->readBytes(buffer, 32);
-
-        // get checksum ready
-        for (uint8_t i=0; i<30; i++) {
-            sum += buffer[i];
-        }
-
-  /* debugging
-  for (uint8_t i=2; i<32; i++) {
-    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
-  }
-  Serial.println();
-  */
-
-    // The data comes in endian'd, this solves it so it works on all platforms
-    uint16_t buffer_u16[15];
-    for (uint8_t i=0; i<15; i++) {
-        buffer_u16[i] = buffer[2 + i*2 + 1];
-        buffer_u16[i] += (buffer[2 + i*2] << 8);
-    }
-
-    // put it into a nice struct :)
-    memcpy((void *)&data, (void *)buffer_u16, 30);
-
-    if (sum != data.checksum) {
-        Serial.println("Checksum failure");
-        return false;
-    }
-  // success!
-  return true;
-}
-
-    pms5003data data;
-    SoftwareSerial pmsSerial;
 };
